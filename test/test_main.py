373d095dfdf32546c5e47196ae662eba15912398
cmatthews20 <cmatthews20@mun.ca>
2024-08-26 23:04:49 -0400

    pytest init, add basic test

---
+++ test/test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from click.testing import CliRunner
+from src.main import main
+
+
+def test_main():
+    """test main entrypoint with basic input"""
+    runner = CliRunner()
+    result = runner.invoke(main, ["--msg=sample message"])
+    assert result.exit_code == 0
+    assert result.output == "sample message\n"
