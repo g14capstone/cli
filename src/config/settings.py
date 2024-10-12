58dfc41b44931f087d7970ee8d8d00200285388f
ShairYousuf <syjahin@mun.ca>
2024-10-12 19:03:02 -0400

    remove api key env

--- src/config/settings.py
+++ src/config/settings.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 # API settings
 API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
-API_KEY = os.getenv("API_KEY")
 
 # CLI settings
 CLI_VERSION = os.getenv("CLI_VERSION", "0.1.0")
