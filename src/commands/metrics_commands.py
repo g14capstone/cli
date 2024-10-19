deb3100ff8de43eb2c9ff475b07fb12b6a209a3f
cmatthews20 <cmatthews20@mun.ca>
2024-10-18 21:28:10 -0400

    src/commands

---
+++ src/commands/metrics_commands.py
@@ -0,0 +1,14 @@
+import click
+from src.commands.base_command import BaseCommand
+
+
+class MachineMetricsCommand(BaseCommand):
+    def execute(self):
+        print(f"Function not yet supported.")
+
+
+@click.command()
+@click.pass_context
+def machine_metrics(ctx):
+    cmd = MachineMetricsCommand()
+    cmd.execute()
