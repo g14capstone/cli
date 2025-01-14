476f69cd7abf720d2204065b753bcb894e509e86
K-rolls <keeganrolls@gmail.com>
2025-01-14 13:28:55 -0330

    Adds other GPU/CPU endpoints to machine API interface

--- src/api/machine_api.py
+++ src/api/machine_api.py
@@ -7,6 +7,8 @@ class MachineAPI:
     def __init__(self):
         self.client = APIClient()
 
+    """FPGA REQUESTS"""
+
     @handle_api_errors
     def create_fpga_machine(
         self, machine_name: str, machine_type: str
@@ -17,6 +19,8 @@ class MachineAPI:
         }
         response = self.client.post("machine/fpga", data=data)
         return response
+
+    """GPU REQUESTS"""
 
     @handle_api_errors
     def create_gpu_machine(
@@ -30,6 +34,30 @@ class MachineAPI:
         return response
 
     @handle_api_errors
+    def pull_gpu_model(self, machine_id: str, model_name: str) -> Dict[str, str]:
+        data = {
+            "machine_name": machine_id,
+            "machine_type": model_name,
+        }
+        response = self.client.post("machine/gpu/pull_model", data=data)
+        return response
+
+    @handle_api_errors
+    def delete_gpu_model(self, machine_id: str, model_name: str) -> Dict[str, str]:
+        data = {
+            "machine_name": machine_id,
+            "machine_type": model_name,
+        }
+        response = self.client.delete("machine/gpu/model", data=data)
+        return response
+
+    @handle_api_errors
+    def get_gpu_inference_url(self, machine_id: str) -> Dict[str, str]:
+        return self.client.get(f"machine/gpu/{machine_id}/inference_url")
+
+    """CPU REQUESTS"""
+
+    @handle_api_errors
     def create_cpu_machine(
         self, machine_name: str, machine_type: str
     ) -> Dict[str, str]:
@@ -39,6 +67,28 @@ class MachineAPI:
         }
         response = self.client.post("machine/cpu", data=data)
         return response
+
+    @handle_api_errors
+    def pull_cpu_model(self, machine_id: str, model_name: str) -> Dict[str, str]:
+        data = {
+            "machine_name": machine_id,
+            "machine_type": model_name,
+        }
+        response = self.client.post("machine/cpu/pull_model", data=data)
+        return response
+
+    @handle_api_errors
+    def delete_cpu_model(self, machine_id: str, model_name: str) -> Dict[str, str]:
+        data = {
+            "machine_name": machine_id,
+            "machine_type": model_name,
+        }
+        response = self.client.delete("machine/cpu/model", data=data)
+        return response
+
+    @handle_api_errors
+    def get_cpu_inference_url(self, machine_id: str) -> Dict[str, str]:
+        return self.client.get(f"machine/cpu/{machine_id}/inference_url")
 
     @handle_api_errors
     def list_user_machines(self):
